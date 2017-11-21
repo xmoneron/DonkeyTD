f9199202855a5529761bccf24847fd059a3e298d
Xavier Moneron <xavier.moneron@gmail.com>
2017-11-21 20:15:51 +0100

    FPS à 50

--- dklabyrinthe.py
+++ dklabyrinthe.py
@@ -14,7 +14,8 @@ def main():
     fenetre = pygame.display.set_mode((cote_fenetre, cote_fenetre))
     icone = pygame.image.load(image_icone)
     titre_fenetre = "DK"
-
+    fps = 50
+    clock = pygame.time.Clock()
     pygame.display.set_icon(icone)
     pygame.display.set_caption(titre_fenetre)
 
@@ -26,6 +27,9 @@ def main():
 
     continuer = True
     while continuer:
+        clock.tick(fps)
+
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 continuer = False
